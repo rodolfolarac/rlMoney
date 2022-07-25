d4ece977e6a88ae2cc420b40bbcc3938e38d9cba
Rodolfo Lara <rodolfolara0303>
2022-07-25 13:19:49 -0300

    Add Axios and Fake Api

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          name: "Rodolfo",
+          age: 28,
+          adress: {
+            city: "Marilia",
+            State: "SP",
+          },
+        },
+      ];
+    });
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
