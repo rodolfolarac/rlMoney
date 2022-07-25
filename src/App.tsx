bef965022ab873fa97b612af0e21e3c36dbb512e
Rodolfo Lara <rodolfolara0303>
2022-07-25 13:49:34 -0300

    Added Base Modal with React-Modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+import { useState } from "react";
+
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModal, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Oiii</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
