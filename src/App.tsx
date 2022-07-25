0fbfc2e2774b73961133a62f9d4e11396f8b84a3
Rodolfo Lara <rodolfolara0303>
2022-07-25 09:06:31 -0300

    feat: Added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1> oi</h1>
+      <GlobalStyle />
     </div>
   );
 }
